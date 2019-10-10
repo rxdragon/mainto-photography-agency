34827b60ff52b2e85e88b44714f9fa617574938c
舒紫栋 <shuzidong@qq.com>
2019-10-10 11:25:16 +0800

    update config
    Signed-off-by: 舒紫栋 <shuzidong@qq.com>

--- vue.config.js
+++ vue.config.js
@@ -5,6 +5,10 @@ function resolve(dir) {
 }
 
 module.exports = {
+  publicPath: './',
+  outputDir: 'dist',
+  assetsDir: 'static',
+  indexPath: 'index.html',
   chainWebpack: (config) => {
     config.resolve.alias.set('@', resolve('src'))
   }
