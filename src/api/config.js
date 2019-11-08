c0c8f42f6b13d2c7e8fd7a3263bd6c20e826c6c3
HaPi <maoxiaobo@foxmail.com>
2019-11-08 11:32:03 +0800

    feat: 添加环境变量

--- src/api/config.js
+++ src/api/config.js
@@ -2,8 +2,8 @@
  * 接口域名的管理
  */
 const base = {    
-  url: 'https://api.local.hzmantu.com/project_cloud3rd',
-  manage_auth: 'https://api.local.hzmantu.com/manage_auth',
+  url: `${process.env.VUE_APP_BASE_URL}/project_cloud3rd`,
+  manage_auth: `${process.env.VUE_APP_BASE_URL}/manage_auth`,
 }
 
 export default base
