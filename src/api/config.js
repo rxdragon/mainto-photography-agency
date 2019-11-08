712f88b4f1c4b7a6661db2f9cab60608b0a32f28
HaPi <maoxiaobo@foxmail.com>
2019-11-08 11:17:34 +0800

    test: 修改预发请求地址

--- src/api/config.js
+++ src/api/config.js
@@ -2,8 +2,8 @@
  * 接口域名的管理
  */
 const base = {    
-  url: 'https://project-cloud-1.local.hzmantu.com/project_cloud3rd',
-  manage_auth: 'https://project-cloud-1.local.hzmantu.com/manage_auth',
+  url: 'https://api.local.hzmantu.com/project_cloud3rd',
+  manage_auth: 'https://api.local.hzmantu.com/manage_auth',
 }
 
 export default base
