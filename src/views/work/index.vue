acad33f9f53c42cf8daa5e4abbcbe96b40e8c590
崔佛 <1406211263@qq.com>
2019-12-27 15:24:24 +0800

    fix: 优化上传照片验证

--- src/views/work/index.vue
+++ src/views/work/index.vue
@@ -5,7 +5,7 @@
       <div class="upload">
         <h4><span class="line" /><span>发布照片</span></h4>
         <a-alert v-if="visible" message="温馨提示" description="1、若要进行照片拼接，需选择标签对照片标记；如标签“拼接A”先后标记了照片1、2，则表示这两张照片需先后被拼接在一起" type="info" closable :after-close="handleClose" :style="{textAlign: 'left', marginBottom: '24px'}" />
-        <Upload ref="uploadChild" @sendPhotos="getPhotos" />
+        <Upload ref="uploadChild" @loading="sendLoding" @sendPhotos="getPhotos" />
       </div>
       <div class="order-info">
         <h4><span class="line" /><span>订单信息</span></h4>
@@ -163,6 +163,9 @@ export default {
         this.$message.error(e.data.error_msg)
         this.$emit('loading', false)
       })
+    },
+    sendLoding (state) {
+      this.$emit('loading', state)
     }
   }
 }
